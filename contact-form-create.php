fb4e1e18ff7d14d506a2856f181c7d2afdf86065
David McFadden <podll0711@gmail.com>
2021-05-04 09:56:59 -0500

    netlify

--- contact-form-create.php
+++ contact-form-create.php
@@ -1,24 +1,24 @@
 <?php
     $name = $_POST[ 'name' ];
     $user_email = $_POST    [ 'email' ];
-    $contact_number = $POST [ 'contact number'];
+    $contact_number = $POST [ 'contact_number'];
     $message = $POST [ 'message' ];
 
     $email_from = 'podll0711@gmail.com';
 
     $email_subject = "New User Form Submission";
 
-    $email_body = "User Name: $name.\n".
-                    "User Email: $user_email.\n".
-                        "User Contact Number: $contact_number.\n".
-                            "User Message: $message.\n";
+    $email_body = "User Name: {$name}.\n".
+                    "User Email: {$user_email}.\n".
+                        "User Contact Number: {$contact_number}.\n".
+                            "User Message: {$message}.\n";
     
 
     $to = "podll0711@gmail.com";
 
-    $headers = "From: $email_from \r\n";
+    $headers = "From: {$email_from} \r\n";
 
-    $headers .= "Reply-To: $user_email \r\n";
+    $headers .= "Reply-To: {$user_email} \r\n";
 
     mail($to, $email_subject, $email_body, $headers);
 
